4c75bba779ff3ed82d5577da09cd78cb2521457c
Patch Sobrak-Seaton <patch.sobrakseaton@gmail.com>
2015-02-04 01:43:35 +0000

    Added Socket.io integration

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 /**
  * Webserver
- *
+ * Image Upload
+ * Private Rooms
+ * Live Updating
+ * Material Design
+ * Avatars
+ * Profiles
+ * Password Protected Rooms
+ * Notifications
+ * Typing Status
+ * Message Editing/Deletion
+ * Chat Logs
+ * Maximum Ocupancy of 2
  */
 
-var express = require('express');
-
-var app = express();
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
@@ -15,9 +26,10 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+server.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, port); 
 });
+
